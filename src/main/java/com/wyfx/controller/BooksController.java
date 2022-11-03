f899bbcbd55be7417451b20c3ee5e7a6b9d4e1cd
ludawei-java <mxwh0428@163.com>
2022-11-03 15:54:17 +0800

    two commit

--- src/main/java/com/wyfx/controller/BooksController.java
+++ src/main/java/com/wyfx/controller/BooksController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class BooksController {
     @GetMapping("/getBook")
     public String getBook(){
+        System.out.println("这是第一个SpringBoot 的应用程序！");
         return "hello springBoot";
     }
 }
