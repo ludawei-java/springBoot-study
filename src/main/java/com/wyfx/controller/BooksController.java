4193b1679aa60326ced6ef7f2d15aa389e4d21e7
ludawei-java <mxwh0428@163.com>
2022-11-03 18:47:10 +0800

    测试配置文件的使用

--- src/main/java/com/wyfx/controller/BooksController.java
+++ src/main/java/com/wyfx/controller/BooksController.java
@@ -1,15 +1,30 @@
 package com.wyfx.controller;
 
+import com.wyfx.Bean.MyDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @RestController
 @RequestMapping("/Books")
 public class BooksController {
+    @Value("${tempdir}")
+    private String pathURL;
+     @Autowired
+    private Environment env;
+     @Resource
+     private MyDataSource myDataSource;
     @GetMapping("/getBook")
     public String getBook(){
         System.out.println("这是第一个SpringBoot 的应用程序！");
+        System.out.println("tempdir=>"+pathURL);
+        System.out.println(env.getProperty("server.port"));
+        System.out.println("myDataSource=>"+myDataSource);
         return "hello springBoot";
     }
 }
